5b805e48c1ddc863fe1fb5cb27ac73a380d0ab9b
Bayaman Gapparov <bayamangapparov@gmail.com>
2020-08-04 10:42:14 -0500

    main view

---
+++ RestaurantApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RestaurantApp
+//
+//  Created by Baiaman Gapparov on 7/28/20.
+//  Copyright © 2020 Baiaman Gapparov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
